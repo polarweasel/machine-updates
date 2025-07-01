1fdea94c684a76eb4e1e72cbc57a93ac5fa205d6
Alex Ball <awball@polarweasel.org>
2025-07-01 12:46:47 -0700

    cleanup

--- generated-python-client/swagger_client/api/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-# flake8: noqa
-
-# import apis into api package
-from swagger_client.api.api_info_api import APIInfoApi
-from swagger_client.api.machine_info_api import MachineInfoApi
